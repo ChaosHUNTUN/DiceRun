12ed0eb7e3fdea2de65f1a99b4dde13b183e8285
CH <CH@DESKTOP-114V9S3>
2023-03-19 01:57:40 +0800

    添加项目文件。

---
+++ DiceHepler.cs
@@ -0,0 +1,133 @@
+﻿namespace ConsoleApp1
+{
+    public static class DiceHepler
+    {
+        private static int upSide = 1;
+        private static int downSide = 6;
+        private static int leftSide = 3;
+        private static int rightSide = 4;
+        private static int frontSide = 2;
+        private static int backSide = 5;
+        public static bool isUpSure = false;
+        public static bool isRightSure = false;
+        public static bool isFrontSure = false;
+        public static int UpSide
+        {
+            get => upSide; set
+            {
+                if (value == 0)
+                    value = 6;
+                if (isUpSure) return;
+                downSide = 7 - value;
+                upSide = value;
+
+                isUpSure = true;
+                //从备选左右前后中去除上方及下方值
+                var b = strings.FindIndex(a => a.Contains(value));
+                strings.RemoveAt(b);
+                //整理左右前后顺序
+                if (IsOdd(value))
+                {
+                    indexNum.Add(strings[1][0]);
+                    indexNum.Add(strings[0][0]);
+                    indexNum.Add(strings[1][1]);
+                    indexNum.Add(strings[0][1]);
+
+                }
+                else
+                {
+                    indexNum.Add(strings[0][0]);
+                    indexNum.Add(strings[1][0]);
+                    indexNum.Add(strings[0][1]);
+                    indexNum.Add(strings[1][1]);
+                }
+
+                //当正上方为1时，正面的结果只能为2，3，4，5
+                //当正上方为2时，正面的结果只能为1，3，4，6
+            }
+        }
+        public static bool IsOdd(int a)
+        {
+            return a % 2 == 0;
+        }
+        public static List<List<int>> strings;
+        public static void CreateNewStrings()
+        {
+            strings = new List<List<int>>() {
+            new List<int> { 1,6},
+            new List<int> { 2,5},
+            new List<int> { 3,4}
+            };
+        }
+        public static List<int> indexNum = new List<int>();
+
+
+
+        public static int GetAroundInt(int front)
+        {
+            var number = indexNum.FindIndex(x => x == front);
+            if (number == 0)
+                return indexNum[indexNum.Count - 1];
+            else
+                return indexNum[number - 1];
+
+
+        }
+        /// <summary>
+        /// 1:2,3,5,4,6
+        /// 2:3,1,4,6,5
+        /// 3:1,2,6,5,4
+        /// 4:2,1,5,6,3
+        /// 5:1,3,6,4,2
+        /// 6:3,2,4,5,1
+        /// </summary>
+        public static int DownSide
+        {
+            get => downSide; set
+            {
+                upSide = 7 - value;
+                downSide = value;
+            }
+        }
+        public static int LeftSide
+        {
+            get => leftSide; set
+            {
+                rightSide = 7 - value;
+                leftSide = value;
+            }
+        }
+        public static int RightSide
+        {
+            get => rightSide; set
+            {
+                rightSide = indexNum[value];
+                FrontSide = GetAroundInt(rightSide);
+
+                leftSide = 7 - rightSide;
+                isRightSure = true;
+            }
+        }
+        public static int FrontSide
+        {
+            get => frontSide; set
+            {
+                backSide = 7 - value;
+                frontSide = value;
+            }
+        }
+        public static int BackSide
+        {
+            get => backSide; set
+            {
+                frontSide = 7 - value;
+                backSide = value;
+            }
+        }
+        static int[] ying = new int[] { 1, 2, 3 };
+        public static string isYING(int a)
+        {
+            return ying.Contains(a) ? "0" : "1";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+object Locker = new object();
+object Locker2 = new object();
+Dictionary<string, int> list = new Dictionary<string, int>();
+Dictionary<string, int> list2 = new Dictionary<string, int>();
+int temp = 0;
+int temp2 = 0;
+Random rd = new Random();
+//创建一个1到24的int数组
+Task.Run(() =>
+{
+    while (temp <= 10000)
+    {
+        DiceHepler.CreateNewStrings();
+        DiceHepler.indexNum.Clear();
+        DiceHepler.isUpSure = false;
+        DiceHepler.isRightSure = false;
+        int c = rd.Next(1, 24);
+        DiceHepler.UpSide = c % 6;
+
+        DiceHepler.RightSide = c / 6;
+        string result = DiceHepler.isYING(DiceHepler.UpSide) + DiceHepler.isYING(DiceHepler.FrontSide) + DiceHepler.isYING(DiceHepler.RightSide);
+        if (result == "001")
+            result = "100";
+        if (result == "011")
+            result = "110";
+        if (result == "010")
+            result = "100";
+        if (result == "101")
+            result = "110";
+        lock (Locker)
+        {
+
+            if (list.ContainsKey(result))
+            {
+                list[result]++;
+            }
+            else
+            {
+                list.Add(result, 1);
+            }
+            temp++;
+        }
+        Thread.Sleep(10);
+    }
+
+});
+Task.Run(() =>
+{
+    while (temp2 < 10000)
+    {
+        var s = rd.Next(0, 4);
+        var a = s >= 3 ? 1 : 0;
+        var b = s >= 2 ? 1 : 0;
+        var c = s >= 1 ? 1 : 0;
+
+        string result = a + "" + b + "" + c;
+        if (result == "001")
+            result = "100";
+        if (result == "011")
+            result = "110";
+        if (result == "010")
+            result = "100";
+        if (result == "101")
+            result = "110";
+
+
+        lock (Locker2)
+        {
+
+
+            if (list2.ContainsKey(result))
+            {
+                list2[result]++;
+            }
+            else
+            {
+                list2.Add(result, 1);
+            }
+            temp2++;
+        }
+    }
+
+});
+
+
+
+while (true)
+{
+    Console.WriteLine("list1:");
+    lock (Locker)
+        foreach (var a in list)
+        {
+            double rs = Math.Round((double)(a.Value / (double)temp), 7);
+            Console.WriteLine(a.Key + ":" + (rs).ToString("0%"));
+        }
+    Console.WriteLine();
+    Console.WriteLine("list2:");
+    lock (Locker2)
+        foreach (var a in list2)
+        {
+            double rs = Math.Round((double)(a.Value / (double)temp2), 7);
+            Console.WriteLine(a.Key + ":" + (rs).ToString("0%"));
+        }
+    Console.WriteLine();
+    Thread.Sleep(5000);
+}
